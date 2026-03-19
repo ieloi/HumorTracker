2947a2caeb4d91e67e648623689be495c6b5498c
Ícaro Matheus <icaro.eloi@mobimark.com.br>
2026-03-19 20:13:49 -0300

    style: configurando estilos globais para o aplicativo

---
+++ src/theme/Theme.ts
@@ -0,0 +1,29 @@
+export const Theme = {
+    colors: {
+        background: '#f2f2f2',
+        paper: '#ffffff',
+        text: '#000000',
+        textPlaceholder: '#a1a1a1',
+        primary: '#926dff',
+        primaryText: '#ffffff',
+        error: '#ff0000',
+        highlight: '#e0D53b',
+        backgroundHighlight: 'rgba(224, 213, 59, 0.17)'
+    },
+    fonts: {
+        sizes: {
+            title: 24, 
+            subtitle: 20, 
+            body: 14
+        },
+        family: {
+            regular: "", 
+            bold: "", 
+            italic: ""
+        }
+    },
+    shadows: {
+        default: '0 0 4 0 rgba(0, 0, 0, 0.25)'
+    }
+
+}
