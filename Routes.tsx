40ac6231b3e9e879b256db79def22498c0c66394
Ícaro Matheus <icaro.eloi@mobimark.com.br>
2026-03-19 19:26:33 -0300

    fix: altera exibição de janela das páginas relacionadas a aplicação de forma geral

--- Routes.tsx
+++ Routes.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator, NativeStackNavigationProp } from "@react-na
 import Home from "./src/screens/Home";
 import Detail from "./src/screens/Detail";
 import SetUserName from "./src/screens/SetUserName";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 type TScreenDefinitions = {
     home: undefined,
@@ -16,10 +17,19 @@ const Stack = createNativeStackNavigator<TScreenDefinitions>();
 
 const AppRoutes = () => {
     return <NavigationContainer>
-        <Stack.Navigator initialRouteName="home">
+        <Stack.Navigator initialRouteName="home" 
+            screenOptions={{ headerShown: false }}
+            screenLayout={({ children }) => <SafeAreaView>{children}</SafeAreaView>}
+        >
             <Stack.Screen name="home" component={Home} />
-            <Stack.Screen name="detail" component={Detail} />
-            <Stack.Screen name="setUserName" component={SetUserName} />
+            <Stack.Group screenOptions={{ 
+                presentation: 'formSheet', 
+                sheetAllowedDetents: [0.5, 0.95],
+                sheetCornerRadius: 24           
+            }}>
+                <Stack.Screen name="detail" component={Detail} />
+                <Stack.Screen name="setUserName" component={SetUserName} />
+            </Stack.Group>
         </Stack.Navigator>
     </NavigationContainer>
 }
